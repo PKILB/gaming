78c92843c839178088f694bdef6082987111867c
Payton Boone <paytonboone30@gmail.com>
2023-04-16 18:59:13 -0600

    gonna go hard on this tomorrow

---
+++ gaming.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'https://localhost:7045' : ''
+export const useSockets = false
+export const domain = 'boone-coding.us.auth0.com'
+export const clientId = 'zucuSPuhK9rVj7G1FAciLHdEXaUb6yHb'
+export const audience = 'https://boone-coding.com'
